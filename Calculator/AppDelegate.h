ceed7525eda515c9767c9eb21c6d62d7e9ba3665
locky1218 <locky1218@locky1218deMacBook-Pro.local>
2015-02-06 21:36:09 +0800

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by locky1218 on 15-2-6.
+//  Copyright (c) 2015年 locky1218. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by locky1218 on 15-2-6.
+//  Copyright (c) 2015年 locky1218. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
